71f11f37e63e1a6e0117e950811b3fce9eaef033
WoodManGitHub <woodmanpublic@gmail.com>
2020-10-28 22:43:57 +0800

    Add MD5

---
+++ PixivWallpaperHelper/Pixiv/Utils/MD5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Pixiv.Utils
+{
+    public static class MD5
+    {
+        /// <summary>
+        /// Create MD5 String
+        /// </summary>
+        public static String MD5code(String str)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider md5Hasher = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            Byte[] data = md5Hasher.ComputeHash((new ASCIIEncoding()).GetBytes(str));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
