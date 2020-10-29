72d25ddf05e118849ed5c5830a784ac6681edd5a
WoodManGitHub <woodmanpublic@gmail.com>
2020-10-29 22:44:07 +0800

    Add Image Util

---
+++ PixivWallpaperHelper/Pixiv/Utils/Image.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixivWallpaperHelper.Pixiv.Utils
+{
+    public class Image
+    {
+        public static Bitmap SaveImage(string url)
+        {
+            WebClient client = new WebClient();
+            client.Headers["Referer"] = "https://www.pixiv.net";
+            Stream stream = client.OpenRead(url);
+            Bitmap bitmap = new Bitmap(stream);
+
+            stream.Flush();
+            stream.Close();
+            client.Dispose();
+
+            return bitmap;
+        }
+    }
+}
