1d206a4de86c52eb44ab61246dd8f7c82415ff8d
WoodMan <woodmanpublic@gmail.com>
2020-10-31 18:59:23 +0800

    Add download to file

--- PixivWallpaperHelper/Utils/Image.cs
+++ PixivWallpaperHelper/Utils/Image.cs
@@ -8,15 +8,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PixivWallpaperHelper.Utils
-{
-    public class Image
+{    public class Image
     {
-        public static Bitmap SaveImage(string url)
+        public static Bitmap SaveImage(string url, string path = null)
         {
             WebClient client = new WebClient();
             client.Headers["Referer"] = "https://www.pixiv.net";
             Stream stream = client.OpenRead(url);
             Bitmap bitmap = new Bitmap(stream);
+
+            if (path != null)
+            {
+                FileStream fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                stream.CopyTo(fileStream);
+            }
 
             stream.Flush();
             stream.Close();
