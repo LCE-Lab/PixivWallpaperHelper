fd55282d27b90896b860c87bae3c016a34bd1524
WoodMan <woodmanpublic@gmail.com>
2020-10-31 23:16:11 +0800

    Download images from fallback for now

--- PixivWallpaperHelper/Pixiv/Util/WallpaperFetcher.cs
+++ PixivWallpaperHelper/Pixiv/Util/WallpaperFetcher.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PixivWallpaperHelper.Pixiv.OAuth;
+using PixivWallpaperHelper.Utils;
+using PixivWallpaperHelper.Pixiv.Objects;
 
 namespace PixivWallpaperHelper.Pixiv.OAuth
 {
@@ -17,17 +19,48 @@ namespace PixivWallpaperHelper.Pixiv.OAuth
         }
 
         public async void fetchWallpaper() {
+            IllustList illustList;
             if (!Properties.Auth.Default.KEY_PIXIV_USER_LOGIN) {
-                var fallbackResult = await PublicAPI.Fallback();
+                illustList = await PublicAPI.Fallback();
+
+                // TODO Move out of if
                 decimal count = Properties.Settings.Default.countNum;
+                var fetchNsfw = Properties.Settings.Default.R18Check;
+                var filterPanting = Properties.Settings.Default.paintingCheck;
+                var filterResolution = Properties.Settings.Default.resolutionNum;
+                var originalImage = (filterResolution > 1200) ? true : Properties.Settings.Default.originPictureCheck;
+                var minView = Properties.Settings.Default.viewCountNum;
+                var minCollection = Properties.Settings.Default.collectionNum;
                 int step = 0;
-                
-                foreach (var result in fallbackResult.Illusts)
+
+                foreach (var result in illustList.Illusts)
                 {
                     if (step == count) break;
-                    Console.WriteLine(result.Id);
+                    if (!fetchNsfw && result.SanityLevel >= 4) continue;
+                    if (filterPanting && result.Type != TypeEnum.Illust) continue;
+                    if (minView > result.TotalView || minCollection > result.TotalBookmarks) continue;
+
+                    if (result.PageCount > 1)
+                    {
+                        for (int i = 0; i < result.MetaPages.Length; i++)
+                        {
+                            Page page = result.MetaPages[i];
+                            var url = (originalImage) ? page.ImageUrls.Original : page.ImageUrls.Large;
+                            var finalPath = $"{path}\\{result.Id}_{i}{Path.GetExtension(url)}";
+                            Image.SaveImage(url, finalPath);
+                            // TODO Save artwork infomation (title author url etc)
+                        }
+                    }
+                    else
+                    {
+                        var url = (originalImage) ? result.MetaSinglePage.OriginalImageUrl : result.ImageUrls.Large;
+                        var finalPath = $"{path}\\{result.Id}{Path.GetExtension(url)}";
+                        Image.SaveImage(url, finalPath);
+                        // TODO Save artwork infomation (title author url etc)
+                    }
                     step++;
                 }
+                // End TODO
             }
         }
     }
