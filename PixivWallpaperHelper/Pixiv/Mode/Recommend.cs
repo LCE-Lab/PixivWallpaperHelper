ef2888694058f22217ace8e565fd438919f360ab
WoodMan <woodmanpublic@gmail.com>
2020-11-02 11:27:52 +0800

    Fix Recommend mode

--- PixivWallpaperHelper/Pixiv/Mode/Recommend.cs
+++ PixivWallpaperHelper/Pixiv/Mode/Recommend.cs
@@ -12,7 +12,7 @@ namespace PixivWallpaperHelper.Pixiv.Mode
     public class Recommend
     {
         private static string API = "https://app-api.pixiv.net/v1/illust/recommended";
-        public static async Task<List<Illust>> GetRecommend(string accessToken, decimal count= 0)
+        public static async Task<List<Illust>> GetRecommend(string accessToken)
         {
             var list = new List<Illust>();
 
@@ -21,15 +21,15 @@ namespace PixivWallpaperHelper.Pixiv.Mode
                 { "Authorization", "Bearer " + accessToken } ,
             };
 
-            var json = await (await Request.CreateRequest(MethodType.GET, API, null, headers)).GetResponseStringAsync();
-            var ranking = JsonConvert.DeserializeObject<IllustList>(json);
-
             do
             {
+                var json = await (await Request.CreateRequest(MethodType.GET, API, null, headers)).GetResponseStringAsync();
+                var ranking = JsonConvert.DeserializeObject<IllustList>(json);
+
                 if (ranking.NextUrl != null) API = ranking.NextUrl; else break;
 
                 list.AddRange(ranking.Illusts);
-            } while (list.Count < count);
+            } while (list.Count < Properties.Settings.Default.countNum);
 
             return list;
         }
